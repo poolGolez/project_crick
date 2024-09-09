299434ed6c9cb85e33bdf7c28a64ea13d3dc8cd6
Pool Golez <loop.edward@gmail.com>
2024-09-09 12:25:34 +0800

    Ingest documentations

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+AWS_DOCS_FILE_PATH = "../resources/docs/wellarchitected-framework.pdf"
+AWSDOCS_INDEX = "aws-docs-nomicembedtext"
+EMBEDDING_MODEL_NAME = "nomic-embed-text"
+
+
--- src/ingest.py
+++ src/ingest.py
@@ -1,21 +1,29 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-FILE_PATH = "../resources/docs/wellarchitected-framework.pdf"
+from src.constants import AWSDOCS_INDEX, EMBEDDING_MODEL_NAME, AWS_DOCS_FILE_PATH
+
+load_dotenv()
 
 
-def run():
-    loader = PyPDFLoader(FILE_PATH)
+def run(file_path: str):
+    loader = PyPDFLoader(file_path)
     docs = loader.load()
-    print(f"Loaded {len(docs)} documents from {FILE_PATH}.")
+    print(f"Loaded {len(docs)} documents from {file_path}.")
 
     recursive_splitter = RecursiveCharacterTextSplitter(chunk_size=650, chunk_overlap=150)
     chunks = recursive_splitter.split_documents(docs)
     print(f"\t> Generated {len(chunks)} chunks.")
 
-    print("Done.")
+    print("Starting embeddings")
+    embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL_NAME)
+    PineconeVectorStore.from_documents(chunks, embeddings, index_name=AWSDOCS_INDEX)
+    print(f"\t> Added {len(chunks)} to Pinecone")
 
 
 if __name__ == "__main__":
-    run()
+    run(AWS_DOCS_FILE_PATH)
     print("All done ✨🍰 ✨!")
